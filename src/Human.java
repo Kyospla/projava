d1aac6dd499a0d5070590453acaeee0841621002
kyou <kyosou0302@icloud.com>
2022-07-11 00:07:09 +0900

    Humanクラスのstatic変数を非staticに変更

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,13 @@
 public class Human {
-    protected static String name;
+    protected String name;
     protected int age;
     protected String tell;
     Human(String name, int age, String habit){
-        Human.name=name;
+        this.name=name;
         this.age=age;
         this.tell=habit;
     }
     public void printInfo(){
-        System.out.printf("名前:%s 歳:%d歳\n 口癖:%s", Human.name,this.age,this.tell);
+        System.out.printf("名前:%s 歳:%d歳\n 口癖:%s", this.name,this.age,this.tell);
     }
 }
